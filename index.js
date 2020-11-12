f726746c6597fc49d3b83770e0a585ee25cef1da
blogame-cre8 <u8fb5kk9vj13-html@yahoo.co.jp>
2020-11-12 17:09:13 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+const { info } = require('console');
+const http = require('http');
+const pug = require('pug');
+const server = http.createServer((req, res) => {
+  const now = new Date();
+  console.info('[' + now + '] Requested By ' + req.connection.remoteAddress);
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
+  switch (req.method) {
+    case 'GET':
+      if (req.url === '/enquetes/yaki-shabu') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '焼肉',
+          secondItem: 'しゃぶしゃぶ'
+        }));
+      } else if (req.url === '/enquetes/rice-bread') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'ごはん',
+          secondItem: 'パン'
+        }));
+      }
+      res.end();
+      break;
+    case 'POST':
+      let rawData = '';
+      req
+        .on('data', chunk => {
+          rawData = rawData + chunk;
+        })
+        .on('end', () => {
+          const qs = require('querystring');
+          const answer = qs.parse(rawData);
+          const body = answer['name'] + 'さんは' + answer['favorite'] + 'に投票しました';
+          console.log('[' + now + '] ' + body);
+          res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + body + '</h1></body></html>');
+          res.end();
+        });
+      break;
+    default:
+      break;
+  }
+}).on('error', e => {
+  console.log('[' + new Date() + '] Server Error', e);
+}).on('clientError', e => {
+  console.log('[' + new Date() + '] Client Error', e);
+});
+
+const port = 8000;
+server.listen(port, () => {
+  console.log('[' + new Date() + '] Listening on ' + port);
+});
